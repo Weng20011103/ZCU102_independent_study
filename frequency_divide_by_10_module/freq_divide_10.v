5033e50908213d4c666e8b0dd512c8d73ea3d402
Weng20011103 <ollie200811@gmail.com>
2023-10-12 16:57:27 +0800

    add frequency divide by 10 module

---
+++ frequency_divide_by_10_module/freq_divide_10.v
@@ -0,0 +1,34 @@
+module freq_div_10(
+    input clk, reset,
+    output reg clk_div_10
+);
+    // Define a 4-bit counter to keep track of the division.
+    reg [3:0]counter4;
+
+    // This always block triggers on the rising edge of the 'clk' signal or a low-level 'reset' signal.
+    always @(posedge clk or negedge reset) begin
+        // If 'reset' is active (low), reset the counter to zero.
+        if (!reset)
+            counter4 <= 4'b0;
+        // If the counter reaches 9, reset it to zero.
+        else if (counter4 == 4'd9)
+            counter4 <= 4'b0;
+        // If none of the above conditions are met, increment the counter by 1.
+        else
+            counter4 <= counter4 + 1'b1;
+    end
+
+    // This always block also triggers on the rising edge of 'clk' or a low-level 'reset' signal.
+    always @(posedge clk or negedge reset) begin
+        // If 'reset' is active (low), the output signal 'clt_div_10' follows the 'clk' input.
+        if (!reset)
+            clk_div_10 <= 1'b0;
+        // If the counter reaches 9, toggle the output signal 'clt_div_10'.
+        else if (counter4 == 4'd9)
+            clk_div_10 <= ~clk_div_10;
+        // If none of the above conditions are met, the output signal 'clt_div_10' follows the 'clk' input.
+        else
+            clk_div_10 <= clk_div_10;
+    end
+
+endmodule
---
+++ frequency_divide_by_10_module/freq_divide_10_tb.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+
+module freq_div_10_tb();
+
+    // Inputs
+    reg clk;          // Clock input signal
+    reg reset;        // Reset signal
+
+    // Outputs
+    wire clk_div_10;  // Output signal from the tested module
+
+    // Instantiate the freq_divide_1000 module (assuming it's the unit under test)
+    freq_div_10 uut (
+        .clk(clk),
+        .reset(reset),
+        .clk_div_10(clk_div_10)
+    );
+
+    // Initial block
+    initial begin
+        // Initialize inputs
+        clk = 0;      // Initialize the clock signal to 0
+        reset = 0;    // Initialize the reset signal to 0
+
+        // Apply reset after a delay of 100 time units
+        #100 reset = 1;
+
+        #3000;  // Wait for additional 2000 time units
+
+        // Finish simulation
+        $finish;  // Terminate the simulation
+    end
+
+    // Clock generation
+    always begin
+        #25 clk = ~clk;  // Toggle the clock signal every 25 time units
+    end
+
+endmodule
