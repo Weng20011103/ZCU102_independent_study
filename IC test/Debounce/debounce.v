b586a62c64fc92c15df91aa0397a3dff9db6b0a3
unknown <ollie200811@gmail.com>
2024-02-25 22:33:43 +0800

    2024/2/25 22:33

--- IC test/Debounce/debounce.v
+++ IC test/Debounce/debounce.v
@@ -1,15 +1,24 @@
 module Debounce(
-    input  clk_1M,      // clk is 1 MHz, 1 µs
+    input  clk_1k,      // clk is 1 kHz, 1 ms
     input  locked,      // Clocking Wizard, locked = 0 is unsafe, locked = 1 is safe
     input button_in,
     output reg button_out
 );
 
-    always@(posedge clk_1M) begin
+    reg [3:0] state;
+    reg [3:0] nextstate;
+
+    always@(posedge clk_1k) begin
         if(!locked)
-            button_out <= 8'd0;
+            state <= 4'd0;
+        else if(button_in)
+            state <= state + 1'b1;
         else
-            button_out <= button_in;
+            state <= 4'd0;
+    end
+
+    always@(*) begin
+        button_out = (state == 4'd15) ? 1'b1 : 1'b0;
     end
 
 endmodule
