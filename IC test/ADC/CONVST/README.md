f19c7104e1dad5240733094434abef03c9492115
unknown <ollie200811@gmail.com>
2024-03-02 02:51:47 +0800

    2024/3/2 02:51

---
+++ IC test/ADC/CONVST/README.md
@@ -0,0 +1,6 @@
+由Keysight 33600A Series Trueform Waveform Generators產生波形  
+  
+![CONVST_1.png](CONVST_1.png "CONVST_1.png")  
+![CONVST_2.png](CONVST_2.png "CONVST_2.png")  
+![CONVST_3.png](CONVST_3.png "CONVST_3.png")  
+  
---
+++ IC test/ADC/CONVST/code/2.py
@@ -0,0 +1,38 @@
+import csv
+import matplotlib.pyplot as plt
+
+# Function to read CSV data skipping the first three rows
+def read_csv(file_path):
+    with open(file_path, 'r') as file:
+        # Skip the first three rows
+        for _ in range(2):
+            next(file)
+        # Read the rest of the data
+        reader = csv.reader(file)
+        data = list(reader)
+    return data
+
+# Function to plot CSV data
+def plot_csv(data):
+    time_values = [float(row[0]) * 1e9 for row in data]
+    ch1_values = [float(row[1]) * 1 for row in data]
+
+    plt.plot(time_values, ch1_values, color='blue', marker='o', markersize=0.1)
+    plt.xlabel('Time (ns)')
+    plt.ylabel('Voltage (V)')
+    plt.title('Channel 1: CONVST')
+    plt.grid(True)
+
+    plt.title('ADC, 2.png, 2.csv')
+
+    # Display the plot
+    plt.show()
+
+# Specify the path to your CSV file
+csv_file_path = '2.csv'
+
+# Read CSV data
+csv_data = read_csv(csv_file_path)
+
+# Plot the data
+plot_csv(csv_data)
