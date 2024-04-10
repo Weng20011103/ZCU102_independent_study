61180fb8b35b3ed544804b7410b369dbfe4b8015
Weng20011103 <ollie200811@gmail.com>
2024-04-10 21:41:41 +0800

    更改 BL_MUX 的 Verilog 程式

--- Verilog/BL_MUX/BL_MUX.v
+++ Verilog/BL_MUX/BL_MUX.v
@@ -2,23 +2,13 @@ module BL_MUX(
     input Reset,
     input EN_in,
     input A2_in, A1_in, A0_in,
-    output reg A2_out, A1_out, A0_out,
-    output reg EN_out
+    output A2_out, A1_out, A0_out,
+    output EN_out
 );
 
-    always @(*) begin
-        if (Reset == 0) begin
-            EN_out = 0;
-            A2_out = 0;
-            A1_out = 0;
-            A0_out = 0;
-        end
-        else begin
-            EN_out = EN_in;
-            A2_out = A2_in;
-            A1_out = A1_in;
-            A0_out = A0_in;
-        end
-    end
+    assign EN_out = (Reset == 1'b0) ? 1'b0 : EN_in;
+    assign A2_out = (Reset == 1'b0 || EN_in == 1'b0) ? 1'b0 : A2_in;
+    assign A1_out = (Reset == 1'b0 || EN_in == 1'b0) ? 1'b0 : A1_in;
+    assign A0_out = (Reset == 1'b0 || EN_in == 1'b0) ? 1'b0 : A0_in;
 
 endmodule
