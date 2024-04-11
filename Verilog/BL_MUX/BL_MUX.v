9c364b059811918fff44b85b69726cbbf71209cf
Weng20011103 <ollie200811@gmail.com>
2024-04-11 23:47:05 +0800

    更新 BL_MUX.v 檔

--- Verilog/BL_MUX/BL_MUX.v
+++ Verilog/BL_MUX/BL_MUX.v
@@ -1,14 +1,17 @@
 module BL_MUX(
     input Reset,
-    input EN_in,
-    input A2_in, A1_in, A0_in,
+    input [3:0] control_signal,
     output A2_out, A1_out, A0_out,
     output EN_out
 );
+    // control_signal[3] is EN
+    // control_signal[2] is A2
+    // control_signal[1] is A1
+    // control_signal[0] is A0
 
-    assign EN_out = (Reset == 1'b0) ? 1'b0 : EN_in;
-    assign A2_out = (Reset == 1'b0 || EN_in == 1'b0) ? 1'b0 : A2_in;
-    assign A1_out = (Reset == 1'b0 || EN_in == 1'b0) ? 1'b0 : A1_in;
-    assign A0_out = (Reset == 1'b0 || EN_in == 1'b0) ? 1'b0 : A0_in;
+    assign EN_out = (Reset == 1'b0) ? 1'b0 : control_signal[3];
+    assign A2_out = (Reset == 1'b0 || EN_in == 1'b0) ? 1'b0 : control_signal[2];
+    assign A1_out = (Reset == 1'b0 || EN_in == 1'b0) ? 1'b0 : control_signal[1];
+    assign A0_out = (Reset == 1'b0 || EN_in == 1'b0) ? 1'b0 : control_signal[0];
 
 endmodule
