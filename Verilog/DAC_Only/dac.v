0c05574ba1e49ceb8f11ff59ad4dffbc62cd30fe
Weng20011103 <144203566+Weng20011103@users.noreply.github.com>
2024-04-24 18:02:36 +0800

    更新 .v 檔

--- Verilog/DAC_Only/dac.v
+++ Verilog/DAC_Only/dac.v
@@ -20,11 +20,14 @@ module DAC7611P(
     always@(*) begin
         case(state)
             10'd1023:  nextstate = 10'd0;
+            10'd499:  nextstate = 10'd0;
             default: nextstate = state + 1'd1;
         endcase
     end
 
-    // CLR_R, dac_signals_15[3]	
+    // CLR_R, dac_signals_15[3]
+    // CLK Width HIGH 30 ns min
+    // CLK Width LOW 30 ns min
     always@(*) begin
         case(state)
             10'd0: dac_signals_15[3] = 1'b1;
@@ -68,23 +71,42 @@ module DAC7611P(
         endcase
     end
 
-    // SDI_R, dac_signals_15[2]	
+    // SDI_R, dac_signals_15[2]
+    // Data Setup 15 ns min
+    // Data Hold 15 ns min
     always@(*) begin
         case(state)
             10'd0: dac_signals_15[2] = 1'b0;
+            10'd1, 10'd2, 10'd3, 10'd4: dac_signals_15[3] = 1'b0; // D11, MSB
+            10'd5, 10'd6, 10'd7, 10'd8: dac_signals_15[3] = 1'b1; // D10
+            10'd9, 10'd10, 10'd11, 10'd12: dac_signals_15[3] = 1'b0; // D9
+            10'd13, 10'd14, 10'd15, 10'd16: dac_signals_15[3] = 1'b1; // D8
+            10'd17, 10'd18, 10'd19, 10'd20: dac_signals_15[3] = 1'b0; // D7
+            10'd21, 10'd22, 10'd23, 10'd24: dac_signals_15[3] = 1'b1; // D6
+            10'd25, 10'd26, 10'd27, 10'd28: dac_signals_15[3] = 1'b0; // D5
+            10'd29, 10'd30, 10'd31, 10'd32: dac_signals_15[3] = 1'b1; // D4
+            10'd33, 10'd34, 10'd35, 10'd36: dac_signals_15[3] = 1'b0; // D3
+            10'd37, 10'd38, 10'd39, 10'd40: dac_signals_15[3] = 1'b1; // D2
+            10'd41, 10'd42, 10'd44, 10'd45: dac_signals_15[3] = 1'b0; // D1
+            10'd46, 10'd47, 10'd48, 10'd49: dac_signals_15[3] = 1'b1; // D0
             default: dac_signals_15[2] = 1'b1;
         endcase
     end
 
-    // LD_R	, dac_signals_15[1]	
+    // LD_R	, dac_signals_15[1]
+    // LD rise to 1 must before the first CLK rise 15 ns min
+    // LD down to 0 must after the last CLK rise 10 ns min
+    // LD Load Pulse Width 20 ns min
     always@(*) begin
         case(state)
             10'd0: dac_signals_15[1] = 1'b1;
+            10'd51, 10'd52: dac_signals_15[1] = 1'b0; // Load Data
             default: dac_signals_15[1] = 1'b1;
         endcase
     end
 
-    // CLR_R, dac_signals_15[0]	
+    // CLR_R, dac_signals_15[0]
+    // CLR Clear Pulse Width 30 ns min
     always@(*) begin
         case(state)
             10'd0: dac_signals_15[0] = 1'b0;
--- Verilog/DAC_Only/fsm_c.v
+++
@@ -1,6 +0,0 @@
-module fsm(
-    input clk_10MHz, locked, reset,
-    output [3:0]dac_signals_15,
-);
-    assign dac_signals_4 = 4'b1111;
-endmodule
