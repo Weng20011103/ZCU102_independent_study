8c1c3b3a17db25a066f72c8e103d458d6b47d75f
Weng20011103 <144203566+Weng20011103@users.noreply.github.com>
2024-04-26 06:55:38 +0800

    更新 state 說明

---
+++ PCB 測試/DAC 波形測試/DAC_U1015/plot2.py
@@ -0,0 +1,71 @@
+import csv
+import matplotlib.pyplot as plt
+
+# Function to read CSV data skipping the first three rows
+def read_csv(file_path):
+    with open(file_path, 'r') as file:
+        # Skip the first four rows
+        for _ in range(2):
+            next(file)
+        # Read the rest of the data
+        reader = csv.reader(file)
+        data = list(reader)
+    return data
+
+# Function to plot CSV data
+def plot_csv(data):
+    time_values = [float(row[0]) * 1e9 for row in data]
+    ch1_values = [float(row[1]) * 1 for row in data]
+    ch2_values = [float(row[2]) * 1 for row in data]
+    ch3_values = [float(row[3]) * 1 for row in data]
+    ch4_values = [float(row[4]) * 1 for row in data]
+
+    # Creating subplots for each channel
+    plt.figure(figsize=(12, 8))
+
+    # Subplot for Channel 1
+    plt.subplot(2, 1, 1)
+    plt.plot(time_values, ch1_values, color='blue', marker='o', markersize=0.1)
+    plt.ylabel('Voltage (V)')
+    plt.title('Channel 1: CLK')
+    plt.grid(True)
+    plt.gca().yaxis.grid(False, which='both')  # Only vertical lines
+    plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)  # Remove x-axis labels
+
+    for i in range (20):
+        plt.text(1825+50*i, 1, f'{37+i}', color='black', ha='center', va='center', fontsize=15)
+
+    # Subplot for Channel 2
+    plt.subplot(2, 1, 2)
+    plt.plot(time_values, ch2_values, color='green', marker='o', markersize=0.1)
+    plt.ylabel('Voltage (V)')
+    plt.title('Channel 2: LD')
+    plt.xlabel('Time (ns)')
+    plt.ylabel('Voltage (V)')
+    plt.grid(True)
+    # Disable horizontal lines
+    plt.gca().yaxis.grid(False, which='both')  # Only vertical lines
+    
+    for i in range (20):
+        plt.text(1825+50*i, 1, f'{37+i}', color='black', ha='center', va='center', fontsize=15)
+
+    # Adjust layout
+    plt.tight_layout()
+
+    # Adjust spacing between subplots and big title
+    plt.subplots_adjust(top=0.9)
+
+    # Add a big title to the entire figure
+    plt.suptitle('DAC U1015', x=0.5)
+
+    # Display the plot
+    plt.show()
+
+# Specify the path to your CSV file
+csv_file_path = 'scope_9.csv'
+
+# Read CSV data
+csv_data = read_csv(csv_file_path)
+
+# Plot the data
+plot_csv(csv_data)
--- PCB 測試/DAC 波形測試/README.md
+++ PCB 測試/DAC 波形測試/README.md
@@ -7,6 +7,7 @@
 輸入時鐘使用`20 MHz`，輸出至 DAC 的 CLK 頻率為 `5 MHz`。  
   
 其中每一個`state`所佔時長為`50 ns`。  
+![state.png](DAC_U1015/Figure_2.png "state.png")   
   
 # 測試結果  
 ![DAC_U1004.png](DAC_U1004/Figure_1.png "DAC_U1004.png")  
